c961708c94aee25b4cb0946d631895ec8267499d
Arda Aksu <79632956+aarda55@users.noreply.github.com>
2023-09-21 21:49:57 +0200

    Create jpgtoschem.java

---
+++ jpgtoschem.java
@@ -0,0 +1,33 @@
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class jpgtoschem {
+ public Material findClosestBlockMaterial(int red, int green, int blue) {
+        Material closestMaterial = null;
+        double closestDistance = Double.MAX_VALUE;
+        
+        for (Material material : Material.values()) {
+            if (material.isBlock() && material.isSolid()) {
+                BlockData blockData = material.createBlockData();
+                int blockRed = blockData.getColor().asRGB() >> 16 & 0xFF;
+                int blockGreen = blockData.getColor().asRGB() >> 8 & 0xFF;
+                int blockBlue = blockData.getColor().asRGB() & 0xFF;
+                
+                double distance = Math.sqrt(
+                        Math.pow(red - blockRed, 2) +
+                        Math.pow(green - blockGreen, 2) +
+                        Math.pow(blue - blockBlue, 2)
+                );
+                
+                if (distance < closestDistance) {
+                    closestDistance = distance;
+                    closestMaterial = material;
+                }
+            }
+        }
+        return closestMaterial;
+    }
+}
